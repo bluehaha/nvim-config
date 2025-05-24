8560b67f4267fc70aaf3d448ce990356b67b3687
bluehaha <laiyushing@gmail.com>
2025-05-24 13:23:29 +0800

    use global cspell json

--- lua/plugins/cspell.lua
+++ lua/plugins/cspell.lua
@@ -11,11 +11,16 @@ return {{
       ensure_installed = { "cspell" }, -- `mason-null-ls` will manage `cspell` as a null-ls source
     })
 
+    local config = {
+      config_file_preferred_name = 'cspell.json',
+      cspell_config_dirs = { "~/.config/" },
+    }
+
     local cspell = require('cspell')
     require("null-ls").setup {
         sources = {
-            cspell.diagnostics,
-            cspell.code_actions,
+            cspell.diagnostics.with({ config = config }),
+            cspell.code_actions.with({ config = config }),
         }
     }
   end
