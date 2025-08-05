d4bc7cdc82ef9844bc7fa8fc6babbf1943f720e4
bluehaha <laiyushing@gmail.com>
2025-08-05 10:51:09 +0800

    feat: add a shortcut to toggle cspell plugin

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -13,3 +13,25 @@ vim.keymap.set('v', '<C-J>', ":m'>+<CR>`<my`>mzgv`yo`z")
 vim.keymap.set('n', '<C-H>', ':bprevious<CR>') -- previous buffer
 vim.keymap.set('n', '<C-L>', ':bnext<CR>')     -- next buffer
 vim.keymap.set('n', '<Leader>w', ':bp|bd #<CR>') -- go to previous buffer & close the last buffer
+
+-- toggle cspell
+vim.keymap.set('n', '<Leader>cs', function()
+  local null_ls = require("null-ls")
+  local sources = null_ls.get_sources()
+  local cspell_active = false
+  
+  for _, source in ipairs(sources) do
+    if source.name == "cspell" then
+      cspell_active = true
+      break
+    end
+  end
+  
+  if cspell_active then
+    null_ls.disable("cspell")
+    print("CSpell disabled")
+  else
+    null_ls.enable("cspell")
+    print("CSpell enabled")
+  end
+end, { desc = "Toggle CSpell" })
