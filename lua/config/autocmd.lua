d6add22846637ff7dcd20452b260be4b52147c55
bluehaha <laiyushing@gmail.com>
2025-10-30 00:15:56 +0800

    feat: change comment type in php file

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.cmd([[%s/\s\+$//e]])
   end,
 })
+
+-- set PHP commentstring to use // instead of /* */
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "php",
+  callback = function()
+    vim.bo.commentstring = "// %s"
+  end,
+})
